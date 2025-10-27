ea004020522d9d03b3f2fb5d403d203297b45f6c
Alfaraz alam <alfarazalam2002@gmail.com>
2025-10-27 10:13:02 +0530

    update feature

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -2,11 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./constants";
 
 const axiosInstance = axios.create({
-<<<<<<< HEAD
   baseURL: BASE_URL,
-=======
-  baseURL: "https://notes-app-qyxw.onrender.com",
->>>>>>> 7c66753dee6b91fbd110c485760a80e2282e25dd
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
