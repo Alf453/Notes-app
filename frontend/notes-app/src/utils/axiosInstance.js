aa1b291766f349eac8af1662caec979e9b6003cf
ALFARAZ ALAM <alfarazalam2002@gmail.com>
2025-10-27 09:52:27 +0530

    Update axiosInstance.js

--- frontend/notes-app/src/utils/axiosInstance.js
+++ frontend/notes-app/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { BASE_URL } from "./constants";
 
 const axiosInstance = axios.create({
-  baseURL: "https://notes-app-beige-tau.vercel.app",
+  baseURL: "https://notes-app-qyxw.onrender.com",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
